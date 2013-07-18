f0c83ad5091342311added2fc4891177c2213245
hailh <black_cat_hh@yahoo.com>
2013-07-18 14:19:26 +0700

    CLEAN

--- 18_7_2013/src/main/java/StringCalculator.java
+++ 18_7_2013/src/main/java/StringCalculator.java
@@ -6,42 +6,35 @@
  * To change this template use File | Settings | File Templates.
  */
 public class StringCalculator {
-    public static final String DEFAULT_DELIMITERS = ",";
+    public static final String DEFAULT_DELIMITERS = ",|\n";
+    public static final String POSTFIX_OF_NEW_DELIMITERS = "//";
+    public static final String SEPARATE_DELIMITERS_AND_NUMBERS = "\n";
+    public static final int MAX_NUMBER = 1000;
     public int add(String numbers) throws NegativeNumberException, UnknownAmountNumberException {
-        try {
-            if(numbers.isEmpty()) {
-                return 0;
-            } else {
-                String delimiter = DEFAULT_DELIMITERS;
-                if(numbers.split("\n")[0].contains("//")){
-                    String delimiterStringPath = numbers.split("\n")[0].replace("//", "");
-                    String[] listDelimiter = delimiterStringPath.substring(1,delimiterStringPath.length()-1).split("\\]\\[");
-                    for(String item : listDelimiter){
-                        delimiter += "|" + item;
-                    }
-                    numbers = numbers.substring(numbers.indexOf("\n") + 1, numbers.length());
+        if(numbers.isEmpty()) {
+            return 0;
+        } else {
+            int total = 0;
+            String delimiter = DEFAULT_DELIMITERS;
+            if(numbers.contains(POSTFIX_OF_NEW_DELIMITERS)){
+                String delimiterStringPath = numbers.split(SEPARATE_DELIMITERS_AND_NUMBERS)[0].replace(POSTFIX_OF_NEW_DELIMITERS, "");
+                String[] listDelimiter = delimiterStringPath.substring(1,delimiterStringPath.length()-1).split("\\]\\[");
+                for(String item : listDelimiter){
+                    delimiter += "|" + item;
                 }
-                if(numbers.split(delimiter).length == 1){
-                    int number = Integer.parseInt(numbers.split(delimiter)[0]);
-                    number = number > 1000 ? 0 : number;
-                    if(number < 0) throw new NegativeNumberException();
-                    return number;
-                } else {
-                    int total = 0;
-                    if(numbers.contains("\n")) {
-                        numbers = numbers.replaceAll("\n", delimiter);
-                    }
-                    String[] listNumber = numbers.split(delimiter);
-                    for(String number : listNumber){
-                        int tempNumber = (Integer.parseInt(number) > 1000 ? 0 : Integer.parseInt(number));
-                        if(tempNumber < 0) throw new NegativeNumberException();
-                        total += tempNumber;
-                    }
-                    return total;
-                }
+                numbers = numbers.substring(numbers.indexOf(SEPARATE_DELIMITERS_AND_NUMBERS) + 1, numbers.length());
             }
-        } catch (Exception exception) {
-            throw new UnknownAmountNumberException(exception);
+            try {
+                String[] listNumber = numbers.split(delimiter);
+                for(String number : listNumber){
+                    int tempNumber = (Integer.parseInt(number) > MAX_NUMBER ? 0 : Integer.parseInt(number));
+                    if(tempNumber < 0) throw new NegativeNumberException();
+                    total += tempNumber;
+                }
+                return total;
+            } catch (Exception exception) {
+                throw new UnknownAmountNumberException(exception);
+            }
         }
     }
 }
