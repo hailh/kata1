1243d8479180b3bb4a88851513c68c8edf1c4f05
hailh <black_cat_hh@yahoo.com>
2013-07-23 08:45:39 +0700

    GREEN test 2

--- 23_7_2013/src/main/java/StringCalculator.java
+++ 23_7_2013/src/main/java/StringCalculator.java
@@ -6,10 +6,23 @@
  * To change this template use File | Settings | File Templates.
  */
 public class StringCalculator {
+    public static final String DEFAULT_DELIMITERS = ",|\n";
+    public static final String POSTFIX_OF_NEW_DELIMITERS = "//";
+    public static final String SEPARATE_DELIMITERS_AND_NUMBERS = "\n";
+    public static final int MAX_NUMBER = 1000;
+
     public int add(String numbers) {
         if(numbers.isEmpty()) {
             return 0;
+        } else {
+            int total = 0;
+            String delimiter = DEFAULT_DELIMITERS;
+                String[] listNumber = numbers.split(delimiter);
+                for(String number : listNumber){
+                    int tempNumber = Integer.parseInt(number);
+                    total += tempNumber;
+                }
+                return total;
         }
-        return -1;
     }
 }
